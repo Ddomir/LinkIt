a03858e83742cc6245a679d1b153486a78cf4090
Brian Daza <bdaza3@uic.edu>
2026-04-07 12:56:11 -0500

    add: single row link cards fit screen

--- frontend/src/components/MainContent.jsx
+++ frontend/src/components/MainContent.jsx
@@ -6,7 +6,7 @@ export default function MainContent({ roomData }) {
         //new card props (roomData) passed in this order:
         //CreateLinkPopup --> Header --> Room --> MainContent
         <div className="flex-1 min-h-0 h-full p-4 overflow-auto">
-            <div className="flex flex-wrap items-start gap-10 justify-start">
+            <div className="flex flex-wrap items-start sm:gap-6 gap-3 justify-start">
                 {Object.entries(roomData?.links || {}).map(([id,link]) => {
                     if (link.type === "folder") {
                         return (
